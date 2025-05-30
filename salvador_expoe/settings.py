49331782caadfb67a645b62840f2e6f5a26c2b74
Lucas Breno Alves dos Santos <lbasantos@tjba.jus.br>
2025-05-30 16:46:28 -0300

    refactor: Carregar SECRET_KEY de variáveis de ambiente

--- salvador_expoe/settings.py
+++ salvador_expoe/settings.py
@@ -5,10 +5,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lbasconceicao.pythonanywhere.com', '127.0.0.1', 'localhost', 'luscas8.pythonanywhere.com']
 
