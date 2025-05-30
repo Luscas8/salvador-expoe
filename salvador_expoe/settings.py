a45967d4198b6fae5f4c1174763f7a7f2bc9bd52
Lucas Breno Alves dos Santos <lbasantos@tjba.jus.br>
2025-05-30 16:40:14 -0300

    feat: Adicionar host do PythonAnywhere aos ALLOWED_HOSTS

--- salvador_expoe/settings.py
+++ salvador_expoe/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lbasconceicao.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['lbasconceicao.pythonanywhere.com', '127.0.0.1', 'localhost', 'luscas8.pythonanywhere.com']
 
 # Application definition
 INSTALLED_APPS = [
